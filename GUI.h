2482fa8111f440e51d281fc7486c2e7a1ba22589
oresto101 <84251772+oresto101@users.noreply.github.com>
2021-12-30 18:20:08 +0200

    Add files via upload

---
+++ GUI.h
@@ -0,0 +1,34 @@
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream> 
+#include <sstream>
+#include "LunarLander.h"
+#include "Terrain.h" 
+#include "Utility.h"
+
+using namespace sf ;
+using namespace std ;
+
+class GUI
+{
+public:
+	GUI(int const& _width, int const& _height, LunarLander *_lander, Terrain *_terrain);
+
+	void draw(RenderWindow &window) ;
+
+private:
+	int width ;
+	int height ;
+	float screenSpacing, textSpacing ;
+	LunarLander *lander ;
+	Terrain *terrain ;
+	Clock timer ;
+
+	Font font ;
+	ostringstream oss ;
+	Text score, time, fuel, altitude, hSpeed, vSpeed, endTxt ;
+};
+
---
+++ Terrain.h
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace sf ;
+using namespace std ;
+
+class Terrain
+{
+public:
+	Terrain(int const& _vertexSpacing,  float const& _terrainSize, Vector2f const& _centerPosition);
+	
+	VertexArray getTerrain() const ;
+	int getVertexSpacing() const ;
+	int getSize() const ;
+	Vector2f getPoint(int const& i) const ;
+	Vector2f getCenterPosition() const ;
+
+	void draw(RenderWindow &window) ;
+
+private:
+	int vertexSpacing ;
+	float terrainSize ;
+	Vector2f centerPosition ;
+	VertexArray terrain ;
+};
